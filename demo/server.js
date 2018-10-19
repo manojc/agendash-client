d5606269c239bd7ff5367530714053c8539c6159
Manoj Chalode <chalodem@gmail.com>
2018-10-19 10:19:11 +0100

    master- updated variable names.

--- demo/server.js
+++ demo/server.js
@@ -2,8 +2,8 @@ const buildApp = require("../lib/app");
 
 buildApp
     .then((app) => {
-        app.listen(process.env.AGENDADH_CLIENT_PORT, () => {
-            console.log(`Agendash Client Started On http://localhost:${process.env.AGENDADH_CLIENT_PORT}`)
+        app.listen(process.env.AGENDASH_CLIENT_PORT, () => {
+            console.log(`Agendash Client Started On http://localhost:${process.env.AGENDASH_CLIENT_PORT}`)
         });
     })
     .catch((error) => console.log(error.stack));
